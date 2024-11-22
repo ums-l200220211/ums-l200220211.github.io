da3ee4024096340666d6008d5d75849f1bb7a637
NAUFALZULHM <naufalmaulanasans.com>
2024-11-22 21:15:58 +0700

    tes Add helloworld.py illustrating the study process

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+def proses_kuliah():
+    print("1. Bayar SPP")
+    print("2. Daftar Mata Kuliah")
+    print("3. Ikut Kelas dan Tugas")
+    print("4. Ujian Akhir")
+    print("5. Mendapatkan Nilai Akhir")
+
+if __name__ == "__main__":
+    print("Proses mengikuti kuliah di Informatika:")
+    proses_kuliah()
